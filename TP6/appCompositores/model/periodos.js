5deea26d6fa854ac06abc3e563ec4e36da860f56
HugoRevuelta <147849214+HugoRevuelta@users.noreply.github.com>
2024-05-28 12:11:47 +0100

    Delete TP6/appCompositores directory

--- TP6/appCompositores/model/periodos.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require("mongoose")
-
-var periodoSchema = new mongoose.Schema({
-    id : String,
-    nome : String
-}, { versionKey: false })
-
-module.exports = mongoose.model('periodos', periodoSchema)
